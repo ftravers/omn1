c5c1c02ada8bd8239ac7f67558505c83dae3e3ee
Fenton Travers <fenton.travers@gmail.com>
2017-04-08 11:54:19 -0700

    one simple *.cljs page app

--- src/cljs/omn1/webpage.cljs
+++ src/cljs/omn1/webpage.cljs
@@ -1,20 +1,40 @@
 (ns omn1.webpage
   (:require
    [om.next :as om :refer-macros [defui]]
-   [om.dom :as dom :refer [div ul li table tr td th thead tbody span]]
-   [goog.dom :as gdom]
-   [omn1.data :as dat]))
+   [om.dom :as dom :refer [div]]
+   [goog.dom :as gdom])
+  (:require-macros
+   [omn1.utils :refer [log]]))
 
-(defui CarRoot
+(defui Greeter
   static om/IQuery
-  (query [_] [:current/car])
+  (query [_] [:name])
 
   Object
   (render
    [this]
-   (let [{{make :car/make} :current/car} (om/props this)]
-     (div
-      nil
-      (div nil (str "Make: ") (dom/b nil make))))))
+   (log "Greeter Component Props" (om/props this))
+   (div nil "Hello " (:name (om/props this)))))
 
-(om/add-root! dat/reconciler CarRoot (gdom/getElement "app"))
+(defmulti reader om/dispatch)
+
+(defmethod reader :default
+  [{st :state} key _]
+  (log "Default Reader Key" key)
+  (log "State" @st)
+  (let [omdb-tree (om/db->tree [key] @st @st)
+        resp {:value (key omdb-tree)}]
+    (log "Om DB->tree" omdb-tree)
+    (log "Responding With" resp)
+    resp))
+
+(def parser (om/parser {:read reader}))
+
+(def app-state (atom {:name "Bob"}))
+
+(def reconciler
+  (om/reconciler
+   {:state app-state
+    :parser parser}))
+
+(om/add-root! reconciler Greeter (gdom/getElement "app"))
