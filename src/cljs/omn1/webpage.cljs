6a2498f5e1829471aa9766e0b7bccd75cfea1ae1
Fenton Travers <fenton.travers@gmail.com>
2017-07-08 11:00:13 -0700

    more

--- src/cljs/omn1/webpage.cljs
+++ src/cljs/omn1/webpage.cljs
@@ -1,12 +1,13 @@
 (ns omn1.webpage
   (:require
-   [omn1.coms :refer [send]]
    [om.next :as om :refer-macros [defui]]
-   [om.dom :as dom :refer [div input p button]]
+   [om.dom :as dom :refer [div]]
    [goog.dom :as gdom]))
 
 (defn log [msg & args]
   (apply js/console.log (conj args (str "[" msg "]:"))))
+
+(defonce app-state (atom {:some-param "not much"}))
 
 (defui Basic
   static om/IQuery
@@ -30,11 +31,11 @@
 (defn my-remoter
   [qry cb]
   (log "remote query" (str qry))
-  (cb {:some-param "value gotten from remote!"}))
+  (log "app state" @app-state)
+  (cb {:some-param "value gotten from remote!"})
+  (log "app state" @app-state))
 
 (def parser (om/parser {:read reader}))
-
-(defonce app-state (atom {:some-param "not much"}))
 
 (def reconciler
   (om/reconciler
