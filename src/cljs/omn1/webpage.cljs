5e265e3d6316b0c13dd6875c0349e0075e479cf5
Fenton Travers <fenton.travers@gmail.com>
2017-07-10 09:29:29 -0700

    simplest om next app possible

--- src/cljs/omn1/webpage.cljs
+++ src/cljs/omn1/webpage.cljs
@@ -1,83 +1,16 @@
 (ns omn1.webpage
   (:require
-   [omn1.coms :refer [send]]
    [om.next :as om :refer-macros [defui]]
-   [om.dom :as dom :refer [div input p button]]
+   [om.dom :as dom :refer [div]]
    [goog.dom :as gdom]))
 
-(defn log [msg & args]
-  (apply js/console.log (conj args (str "[" msg "]:"))))
-
-(defui Login
-  static om/IQuery
-  (query  [_] '[(:user/authenticated {:user/name ?name :user/password ?password})])
-  
-  static om/IQueryParams
-  (params [this]
-          {:name "" :password ""})
-  
+(defui SimpleUI
   Object
-  (initLocalState [this] {:username "fenton" :password "passwErd"})
   (render
    [this]
-   (log "props" (om/props this))
-   (let [{:keys [username password]} (om.next/get-state this)]
-     (if (:user/authenticated (om/props this))
-       (div nil "You are logged in!")
-       (div
-        nil
-        (input #js {:name "uname" :type "text" :placeholder "Enter Username"
-                    :required true :value username
-                    :onChange
-                    (fn [ev]
-                      (let [value (.. ev -target -value)]
-                        (om/update-state! this assoc :username value)))}) (p nil)
-        (input #js {:name "psw" :type "password" :placeholder "Enter Password"
-                    :required true :value password 
-                    :onChange
-                    (fn [ev]
-                      (let [value (.. ev -target -value)]
-                        (om/update-state! this assoc :password value)))}) (p nil)
-        (button
-         #js
-         {:onClick
-          (fn [e]
-            (let [state (om.next/get-state this)]
-              (om.next/set-query!
-               this
-               {:params
-                {:name (:username state)
-                 :password (:password state)}})))} "Login"))))))
+   (div nil "Hello World")))
 
-(defmulti reader om/dispatch)
-
-(defmethod reader :default
-  [{st :state :as env} key _]
-  (let [result (key (om/db->tree [key] @st @st))
-        resp {:value result :remote true} ]
-    (log "reader" {:key key
-                   :env-target (:target env)
-                   :resp resp})
-    resp))
-
-(defn my-cb [cb data]
-  (let [read-data (cljs.reader/read-string data)]
-    (log "got data back" (str read-data))
-    (cb read-data)))
-
-(defn make-remote-req
-  [qry cb]
-  (log "qry" (str qry))
-  (send (partial my-cb cb) (:remote qry)))
-
-(def parser (om/parser {:read reader}))
-
-(defonce app-state (atom {:user/authenticated false}))
-
-(def reconciler
-  (om/reconciler
-   {:state app-state
-    :parser parser
-    :send make-remote-req}))
-
-(om/add-root! reconciler Login (gdom/getElement "app"))
+(om/add-root!
+ (om/reconciler {})
+ SimpleUI
+ (gdom/getElement "app"))
